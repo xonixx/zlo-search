4b5621addfc99023674d52ed07dc5c687270bb7d
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-09-03 22:50:06 +0000

    Cookie support for site drop-down fix for problems with encodings

--- trunk/zlo_web/src/org/xonix/zlo/web/servlets/SearchServlet.java
+++ trunk/zlo_web/src/org/xonix/zlo/web/servlets/SearchServlet.java
@@ -2,12 +2,15 @@ package org.xonix.zlo.web.servlets;
 
 import org.apache.commons.lang.StringUtils;
 import org.xonix.zlo.search.ZloSearcher;
+import org.xonix.zlo.search.Config;
 import org.xonix.zlo.search.model.ZloMessage;
 import org.xonix.zlo.web.servlets.helpful.ForwardingRequest;
 import org.xonix.zlo.web.servlets.helpful.ForwardingServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -24,19 +27,49 @@ public class SearchServlet extends ForwardingServlet {
         String nick = request.getParameter("nick");
         String host = request.getParameter("host");
 
-        if (StringUtils.isNotEmpty(title) |
-                StringUtils.isNotEmpty(body) |
-                StringUtils.isNotEmpty(nick) |
-                StringUtils.isNotEmpty(host) |
+        if (StringUtils.isNotEmpty(title) ||
+                StringUtils.isNotEmpty(body) ||
+                StringUtils.isNotEmpty(nick) ||
+                StringUtils.isNotEmpty(host) ||
                 StringUtils.isNotEmpty(topicCode) && !"0".equals(topicCode)) {
             request.setAttribute("searchResult", ZloSearcher.search(topicCode, title, body, nick, host));
         }
 
         request.setAttribute("debug", "true".equalsIgnoreCase(getServletContext().getInitParameter("debug")));
-        request.setAttribute("siteRoot", "zlo.rt.mipt.ru");
+
+        String siteInCookie;
+        if (StringUtils.isNotEmpty(request.getParameter("site"))){
+            request.setAttribute("siteRoot", Config.SITES[Integer.valueOf(request.getParameter("site"))]);
+            rememberInCookie(response, "site", request.getParameter("site"));
+        } else if (StringUtils.isNotEmpty(siteInCookie = recallFromCookie(request, "site"))){
+            request.setAttribute("site", siteInCookie); // for drop-down
+            request.setAttribute("siteRoot", Config.SITES[Integer.valueOf(siteInCookie)]); // for search result list
+        } else {
+            request.setAttribute("siteRoot", Config.SITES[0]);
+        }
+
         //request.setAttribute("error", "Fuck!!!");
 
-        response.setCharacterEncoding("UTF-8");
+        //response.setCharacterEncoding("UTF-8");
         request.forwardTo("/Search.jsp");
+    }
+
+    private void rememberInCookie(HttpServletResponse response, String fieldname, String value, int age) {
+        Cookie cookie = new Cookie(fieldname, value);
+        cookie.setMaxAge(age); // forever
+        response.addCookie(cookie);
+    }
+
+    private void rememberInCookie(HttpServletResponse response, String fieldname, String value) {
+        rememberInCookie(response, fieldname, value, Integer.MAX_VALUE); // forever    
+    }
+
+    private String recallFromCookie(HttpServletRequest request, String fieldname) {
+        Cookie [] cookies = request.getCookies();
+        for (Cookie cookie: cookies) {
+            if (fieldname.equals(cookie.getName()))
+                return cookie.getValue();
+        }
+        return null;
     }
 }
