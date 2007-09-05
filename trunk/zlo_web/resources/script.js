64c9327c9f1cbd92428d036eff77e7317ff54f2b
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-09-05 22:55:03 +0000

    script.js

---
+++ trunk/zlo_web/resources/script.js
@@ -0,0 +1,7 @@
+
+function changedDatesSelector() {
+    var datesSelector = document.getElementById("dates");
+    document.getElementById("fd").disabled =
+    document.getElementById("td").disabled = !datesSelector.checked;
+    return true;
+}
