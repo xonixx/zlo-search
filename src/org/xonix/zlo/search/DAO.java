8a3c28c151d0566eb8bb8b26e8b5f52b88e5dc61
boost <boost@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-09-13 16:33:11 +0000

    DB store implemented: - added DB scripts (DB dump and DB deploy) - added DBConnector to config - added methods (saveMsgs(), getMessageByNumber())

--- src/org/xonix/zlo/search/DAO.java
+++ src/org/xonix/zlo/search/DAO.java
@@ -1,11 +1,17 @@
 package org.xonix.zlo.search;
 
 import org.xonix.zlo.search.model.ZloMessage;
+import org.xonix.zlo.search.config.Config;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Vector;
 import java.util.ArrayList;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.sql.ResultSet;
 
 /**
  * Author: gubarkov
@@ -45,6 +51,44 @@ public class DAO {
     }
 
     public static class DB {
+        private static String insertPreparedStatement = "INSERT INTO messages(id, topic, title, nick, mdate, body) VALUES (?, ?, ?, ?, ?, ?)";
+
+        private static String selectMessageById = "SELECT * FROM messages WHERE id=?";
+
+        public void saveMsgs(int startNum, int endNum) throws SQLException, IOException {
+        PreparedStatement pstmt = Config.DBCONNECTION.prepareStatement(insertPreparedStatement);
+
+        for (int i=startNum; i<=endNum; i++) {
+            ZloMessage msg = PageParser.parseMessage(PageRetriever.getPageContentByNumber(i), i);
+            if (msg != null) {
+                pstmt.setInt(1, msg.getNum());
+                pstmt.setString(2, msg.getTopic());
+                pstmt.setString(3, msg.getTitle());
+                pstmt.setString(4, msg.getNick());
+                pstmt.setTimestamp(5, new Timestamp(msg.getDate().getTime()));
+                pstmt.setString(6, msg.getBody());
+
+                pstmt.executeUpdate();
+            }
+        }
+    }
+
+    public ZloMessage getMessageByNumber(int count) throws SQLException, UnsupportedEncodingException {
+        PreparedStatement st = Config.DBCONNECTION.prepareStatement(selectMessageById);
+        st.setInt(1, count);
+        ResultSet rs = st.executeQuery();
+        ZloMessage msg = new ZloMessage();
+        while (rs.next()){
+            msg.setNum(rs.getInt("id"));
+            msg.setTopic(rs.getString("topic"));
+            msg.setNick(rs.getString("nick"));
+            msg.setTitle(rs.getString("title"));
+            msg.setTopic(rs.getString("topic"));
+            msg.setDate(rs.getTimestamp("mdate"));
+            msg.setBody(rs.getString("body"));
+        }
+        return msg;
+    }
 
     }
 }
