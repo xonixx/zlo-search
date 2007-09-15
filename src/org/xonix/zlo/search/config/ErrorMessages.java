19bd84221f9c94f4ac6071d3b6dde35171644899
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-09-15 00:39:28 +0000

    Saved msg functionality

--- src/org/xonix/zlo/search/config/ErrorMessages.java
+++ src/org/xonix/zlo/search/config/ErrorMessages.java
@@ -5,14 +5,17 @@ package org.xonix.zlo.search.config;
 * Date: 11.09.2007
 * Time: 17:56:07
 */
-public enum ErrorMsgs {
+public enum ErrorMessages {
     ToDateInvalid("error.toDate"),
     FromDateInvalid("error.fromDate"),
-    MustSelectCriterion("error.must.select.criterion");
+    MustSelectCriterion("error.must.select.criterion"),
+
+    NumParameterInvalid("error.num.param.invalid"),
+    MessageNotFound("error.msg.not.found");
 
     private String val;
 
-    ErrorMsgs(String key) {
+    ErrorMessages(String key) {
         this.val = Config.getProp(key);
     }
 
