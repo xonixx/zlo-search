c5e9a20e51b4ea6a58577b813ab4b53637f0fcf2
boost <boost@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-09-28 16:14:50 +0000

    Added Messages crawling from site to DB, draft version ! hint ! before using fix "null" ZloMessage problem.

--- trunk/src/org/xonix/zlo/search/config/Config.java
+++ trunk/src/org/xonix/zlo/search/config/Config.java
@@ -75,4 +75,7 @@ public class Config {
         }
         DB_CONNECTION = _a;
     }
+
+    public static final int NUM_OF_CONTINIOUSSCAN = Integer.parseInt(getProp("crawler.max_num_to_scan"));
+    public static final int NUM_OF_WAIT_SECONDS = Integer.parseInt(getProp("crawler.wait_seconds"));
 }
