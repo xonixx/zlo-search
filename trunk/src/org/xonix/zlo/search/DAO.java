c45ba8588f64caadcdb8e2e44d03fa38894f7568
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2007-10-01 18:08:50 +0000

    refactor

--- trunk/src/org/xonix/zlo/search/DAO.java
+++ trunk/src/org/xonix/zlo/search/DAO.java
@@ -4,9 +4,7 @@ import org.xonix.zlo.search.config.Config;
 import org.xonix.zlo.search.model.ZloMessage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import java.util.logging.Logger;
 
 /**
@@ -17,7 +15,9 @@ import java.util.logging.Logger;
 
 // data access object
 public class DAO {
+
     private static Logger logger = Logger.getLogger(DAO.class.getName());
+
     public static class Exception extends java.lang.Exception {
         public Exception(Throwable cause) {
             super(cause);
@@ -41,91 +41,8 @@ public class DAO {
             }
         }
 
-        private static class MessageRetriever extends Thread {
-            private static int from = -1;
-            private static int to = -1;
-            private static int currentNum = -1;
-
-            private static int threadNum = 0;
-
-            private List<ZloMessage> msgs;
-
-            public MessageRetriever(int from, int to, List<ZloMessage> msgs) {
-                super("MessageRetriever #" + threadNum);
-                if (MessageRetriever.from == -1)
-                    MessageRetriever.from = from;
-                if (MessageRetriever.to == -1)
-                    MessageRetriever.to = to;
-                if (currentNum == -1)
-                    currentNum = from;
-                this.msgs = msgs;
-                logger.info("Born #" + threadNum);
-                threadNum++;
-            }
-
-            private static boolean hasMoreToDownload() {
-                return currentNum <= to;
-            }
-
-            private int getNextNum() {
-                return currentNum++;
-            }
-
-            public void run() {
-//                System.out.println("Run " + currentNum + ", " + from + ", " + to);
-                try {
-                    while (hasMoreToDownload()) {
-                        try {
-                            logger.info("Downloading " + currentNum);
-                            msgs.add(SOURCE.getMessageByNumber(getNextNum()));
-                        } catch (Exception e) {
-                            e.printStackTrace(); // todo: need to decide what to do here
-                        }
-                    }
-                } finally {
-                    logger.info(getName()+" finished.");
-                    threadNum--;
-
-                    if (threadNum == 0) {
-                        // no more to download - making MessageRetriever ready for next requests
-                        from = to = currentNum = -1;
-                    }
-                }
-            }
-        }
-
-        public List<ZloMessage> getMessages(final int from, final int to, int threadsNum) throws Exception {
-            final List<ZloMessage> msgs;
-            if (threadsNum == 1) {
-                msgs = new ArrayList<ZloMessage>();
-
-                for (int i = from; i <= to; i++) {
-                    msgs.add(getMessageByNumber(i));
-                }
-            } else {
-                msgs = new Vector<ZloMessage>();
-                List<Thread> threads = new ArrayList<Thread>();
-
-                // starting all threads
-                for (int i = 0; i < threadsNum; i++) {
-                    Thread t = new MessageRetriever(from, to, msgs);
-                    t.start();
-                    threads.add(t);
-                }
-
-                // joining them - main thread waits for all
-                try {
-                    for(Thread t: threads)
-                        t.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return msgs;
-        }
-
         public List<ZloMessage> getMessages(int from, int to) throws Exception {
-            return getMessages(from, to, Config.THREADS_NUMBER);
+            return MultithreadedRetriever.getMessages(SOURCE, from, to, Config.THREADS_NUMBER);
         }
 
         public int getLastMessageNumber() throws Exception {
---
+++ trunk/src/org/xonix/zlo/search/MultithreadedRetriever.java
@@ -0,0 +1,104 @@
+package org.xonix.zlo.search;
+
+import org.xonix.zlo.search.model.ZloMessage;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+/**
+ * Author: gubarkov
+ * Date: 01.10.2007
+ * Time: 21:18:54
+ */
+public class MultithreadedRetriever {
+
+    private static Logger logger = Logger.getLogger(MultithreadedRetriever.class.getName());
+
+    private static class MessageRetriever extends Thread {
+        private static int from = -1;
+        private static int to = -1;
+        private static int currentNum = -1;
+
+        private static int threadNum = 0;
+
+        private List<ZloMessage> msgs;
+        private IndexingSource source;
+
+        public MessageRetriever(IndexingSource source, int from, int to, List<ZloMessage> msgs) {
+            super("MessageRetriever #" + threadNum);
+
+            this.source = source;
+
+            if (MessageRetriever.from == -1)
+                MessageRetriever.from = from;
+            if (MessageRetriever.to == -1)
+                MessageRetriever.to = to;
+            if (currentNum == -1)
+                currentNum = from;
+            this.msgs = msgs;
+            logger.info("Born #" + threadNum);
+            threadNum++;
+        }
+
+        private static boolean hasMoreToDownload() {
+            return currentNum <= to;
+        }
+
+        private int getNextNum() {
+            return currentNum++;
+        }
+
+        public void run() {
+            try {
+                while (hasMoreToDownload()) {
+                    try {
+                        logger.info("Downloading " + currentNum);
+                        msgs.add(source.getMessageByNumber(getNextNum()));
+                    } catch (Exception e) {
+                        e.printStackTrace(); // todo: need to decide what to do here
+                    }
+                }
+            } finally {
+                logger.info(getName()+" finished.");
+                threadNum--;
+
+                if (threadNum == 0) {
+                    // no more to download - making MessageRetriever ready for next requests
+                    from = to = currentNum = -1;
+                }
+            }
+        }
+    }
+
+    public static List<ZloMessage> getMessages(IndexingSource source, final int from, final int to, int threadsNum) throws DAO.Exception {
+        final List<ZloMessage> msgs;
+        if (threadsNum == 1) {
+            msgs = new ArrayList<ZloMessage>();
+
+            for (int i = from; i <= to; i++) {
+                msgs.add(source.getMessageByNumber(i));
+            }
+        } else {
+            msgs = new Vector<ZloMessage>();
+            List<Thread> threads = new ArrayList<Thread>();
+
+            // starting all threads
+            for (int i = 0; i < threadsNum; i++) {
+                Thread t = new MessageRetriever(source, from, to, msgs);
+                t.start();
+                threads.add(t);
+            }
+
+            // joining them - main thread waits for all
+            try {
+                for(Thread t: threads)
+                    t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return msgs;
+    }
+}
