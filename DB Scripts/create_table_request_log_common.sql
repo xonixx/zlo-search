cec2746fdd47d6902d3210d82df3d92cac4e582f
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2008-02-18 19:57:57 +0000

    impr search requests logging

--- DB Scripts/create_table_request_log_common.sql
+++ DB Scripts/create_table_request_log_common.sql
@@ -2,12 +2,17 @@ use mysql;
 create database searcher_logs;
 use searcher_logs;
 
+drop table if exists request_log;
+
 CREATE TABLE request_log (
     id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
     site INT,
     host VARCHAR(100),
     user_agent VARCHAR(100),
     req_text VARCHAR(200),
+    req_nick VARCHAR(100),
+    req_host VARCHAR(100),
+    req_query_str VARCHAR(400),
     req_query VARCHAR(200),
     referer VARCHAR(100),
     req_date DATETIME
