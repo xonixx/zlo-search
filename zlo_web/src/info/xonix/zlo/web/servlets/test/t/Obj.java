66fded0a402ca2ff636401604169691734f73f41
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2008-04-30 22:23:43 +0000

    test

---
+++ zlo_web/src/info/xonix/zlo/web/servlets/test/t/Obj.java
@@ -0,0 +1,22 @@
+package info.xonix.zlo.web.servlets.test.t;
+
+public class Obj {
+    private int n;
+    private String s;
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
