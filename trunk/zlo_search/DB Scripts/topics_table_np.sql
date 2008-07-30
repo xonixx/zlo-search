7fbcb4e72061d3e3f5c595d61d9160ca6317e370
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2008-07-30 20:49:50 +0000

    np board added

---
+++ trunk/zlo_search/DB Scripts/topics_table_np.sql
@@ -0,0 +1,20 @@
+
+
+
+USE np_storage;
+
+-- create
+CREATE TABLE topics (id INT,
+                        name CHAR(50),
+                        isNew BOOL
+                    );
+
+-- index
+ALTER TABLE topics
+  ADD INDEX (id);
+
+-- insert
+INSERT INTO topics (id, name, isNew) VALUES (0, 'без темы',         1);
+INSERT INTO topics (id, name, isNew) VALUES (1, ' то здесь?',           1);
+
+-- old
