48606ff5dabd732af6c339ea6494b889e9bd6466
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2009-04-18 23:43:38 +0000

    impr optimize script

--- trunk/zlo_search/scripts/OptimizeAllIndex.bat
+++ trunk/zlo_search/scripts/OptimizeAllIndex.bat
@@ -3,6 +3,16 @@
 set J=j.bat
 
 title OptimizeIndex
+
+echo Stopping services...
+net stop ZSDaemons
+net stop ZSTomcat
+sleep 10
+
 call set_env.bat
 call %J% info.xonix.zlo.search.progs.OptimizeAllIndexes
+
+echo Starting services...
+net start ZSDaemons
+net start ZSTomcat
 pause
