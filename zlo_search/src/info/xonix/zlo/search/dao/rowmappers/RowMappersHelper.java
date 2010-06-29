9a6cebf04a483f3e0d1dbc3f8d0cfed3aa4bd25c
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2010-06-29 23:42:46 +0000

    db impr

--- zlo_search/src/info/xonix/zlo/search/dao/rowmappers/RowMappersHelper.java
+++ zlo_search/src/info/xonix/zlo/search/dao/rowmappers/RowMappersHelper.java
@@ -1,9 +1,25 @@
 package info.xonix.zlo.search.dao.rowmappers;
 
+import info.xonix.zlo.search.utils.factory.Factory;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+
 /**
  * User: Vovan
  * Date: 13.06.2010
  * Time: 1:40:22
  */
 public class RowMappersHelper {
+    private Factory<Class, RowMapper> rowMappersCache = new Factory<Class, RowMapper>() {
+        @Override
+        @SuppressWarnings("unchecked")
+        protected RowMapper create(Class aClass) {
+            return BeanPropertyRowMapper.newInstance(aClass);
+        }
+    };
+
+    @SuppressWarnings("unchecked")
+    public <T> RowMapper<T> beanRowMapper(Class<T> clazz) {
+        return rowMappersCache.get(clazz);
+    }
 }
---
+++ zlo_search/src/info/xonix/zlo/search/model/Topic.java
@@ -0,0 +1,27 @@
+package info.xonix.zlo.search.model;
+
+/**
+ * User: Vovan
+ * Date: 30.06.2010
+ * Time: 3:27:07
+ */
+public class Topic {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
