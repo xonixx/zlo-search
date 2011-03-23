a3ab1ac98e0ef0f0c60a9fa421a344eead6a432d
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2011-03-23 18:26:25 +0000

    reindex sql script

---
+++ zlo_search/DB Scripts/misc/reindex.sql
@@ -0,0 +1,11 @@
+
+update zlo_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update games_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update velo_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update dev_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update x_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update dolgopa_db_dict set int_val=0 where var_name='lastIndexedDouble';
+
+-- update sport_db_dict set int_val=0 where var_name='lastIndexedDouble';
+-- update anime_db_dict set int_val=0 where var_name='lastIndexedDouble';
+-- update takeoff_db_dict set int_val=0 where var_name='lastIndexedDouble';
