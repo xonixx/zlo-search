f89dab6be69c49328baffd42660e9aea47ba49d1
xonix <xonix@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2011-03-23 16:00:29 +0000

    GC admin button

---
+++ trunk/zlo_search/src/main/java/info/xonix/zlo/search/utils/SysUtils.java
@@ -0,0 +1,40 @@
+package info.xonix.zlo.search.utils;
+
+import org.apache.log4j.Logger;
+
+/**
+ * User: Vovan
+ * Date: 23.03.11
+ * Time: 18:29
+ */
+public final class SysUtils {
+    private final static Logger log = Logger.getLogger(SysUtils.class);
+
+    public static void gc() {
+        log.info("Going to GC...");
+
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+
+/*        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();
+        System.gc();*/
+
+        log.info("GC done...");
+    }
+}
