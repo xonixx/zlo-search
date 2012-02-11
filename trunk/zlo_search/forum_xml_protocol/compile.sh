8bc047f1bb4ec9a9b81411bc9db0a28c9c0758f6
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-02-11 17:51:38 +0000

    jaxb upd

---
+++ trunk/zlo_search/forum_xml_protocol/compile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SRC_DIR="D:/stuff/test/java/zlo-search/zlo_search/src/main/java"
+
+JAXB_HOME="D:/DISTR/JAVA_J2EE/JAXB/jaxb-ri-20110601"
+#JAXB_HOME="D:/!DISTRIBs/Programming/Java/xml/jaxb-2_1_7/jaxb-ri"
+
+export JAXB_HOME
+
+JAXB_OPTS="-no-header " # don't add timestamp
+
+OUT_DIR="$SRC_DIR/info/xonix/zlo/search/forumxmlprotocol/xjccompiled"
+
+PATH="$JAXB_HOME/bin:$PATH"
+
+#rm -rf $OUT_DIR
+sh xjc.sh $JAXB_OPTS -p info.xonix.zlo.search.xmlfp.xjccompiled.message -d $SRC_DIR message.xsd
+sh xjc.sh $JAXB_OPTS -p info.xonix.zlo.search.xmlfp.xjccompiled.lastMessageNumber -d $SRC_DIR lastMessageNumber.xsd
+
+#echo "Copying xsd"
+#copy *.xsd D:/TEST/JAVA/ZloSearcher/trunk/zlo_web/resources/xmlfp/xsd
+
+#pause
