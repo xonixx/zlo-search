0a5e905215bbde06b55574842cf37b4361376da3
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-03-13 00:47:58 +0000

    fmlfp xsd validation

--- trunk/zlo_search/src/main/java/info/xonix/zlo/search/xmlfp/XmlFpContext.java
+++ trunk/zlo_search/src/main/java/info/xonix/zlo/search/xmlfp/XmlFpContext.java
@@ -1,9 +1,18 @@
 package info.xonix.zlo.search.xmlfp;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.xml.sax.SAXException;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -19,12 +28,89 @@ class XmlFpContext {
     }
 
     public static Marshaller getMarshaller() {
-        return JaxbMarshallerHolder.MARSHALLER_XMLFP;
+//        return JaxbMarshallerHolder.MARSHALLER_XMLFP;
+        return getValidatingMarshaller();
     }
 
     public static Unmarshaller getUnmarshaller() {
-        return JaxbUnmarshallerHolder.UNMARSHALLER_XMLFP;
+//        return JaxbUnmarshallerHolder.UNMARSHALLER_XMLFP;
+        return getValidatingUnmarshaller();
     }
+
+    private static Marshaller getValidatingMarshaller() {
+        return ValidatingMarshallerHolder.MARSHALLER;
+    }
+
+    private static Unmarshaller getValidatingUnmarshaller() {
+        return ValidatingUnmarshallerHolder.UNMARSHALLER;
+    }
+
+    private static Marshaller newValidatingMarshaller() {
+        final Marshaller marshaller = newMarshaller();
+        marshaller.setSchema(getAllSchemasSchema());
+        return marshaller;
+    }
+
+    private static Unmarshaller newValidatingUnmarshaller() {
+        final Unmarshaller unmarshaller = newUnmarshaller();
+        unmarshaller.setSchema(getAllSchemasSchema());
+        return unmarshaller;
+    }
+
+    private static SchemaFactory schemaFactory = SchemaFactory.newInstance(
+            javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+    private static Schema getAllSchemasSchema() {
+        try {
+            return schemaFactory.newSchema(getXsdSources());
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Source[] getXsdSources() {
+        try {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+            final Resource[] resources;
+            resources = resolver.getResources("info/xonix/zlo/search/xmlfp/xsd/*.xsd");
+//            resources = resolver.getResources("info/xonix/zlo/search/xmlfp/xsd/mess*.xsd");
+
+            final Source[] sources = new Source[resources.length];
+
+            for (int i = 0; i < resources.length; i++) {
+                Resource resource = resources[i];
+                sources[i] = new StreamSource(resource.getFile());
+            }
+
+            return sources;
+        } catch (IOException e) {
+            throw new RuntimeException("getXsdSources", e);
+        }
+    }
+
+
+/*    private static StringFactory<Marshaller> marshallersCache = new StringFactory<Marshaller>() {
+    @Override
+    protected Marshaller create(String xsdPath) {
+        return null;
+    }
+};
+
+private static StringFactory<Unmarshaller> unmarshallersCache = new StringFactory<Unmarshaller>() {
+    @Override
+    protected Unmarshaller create(String xsdPath) {
+        return null;
+    }
+};
+
+public static Marshaller getMarshaller(String xsdPath) {
+    return marshallersCache.get(xsdPath);
+}
+
+public static Unmarshaller getUnmarshaller(String xsdPath) {
+    return unmarshallersCache.get(xsdPath);
+}*/
 
     private static class JaxbContextHolder {
         static final JAXBContext JAXB_CONTEXT;
@@ -39,16 +125,7 @@ class XmlFpContext {
     }
 
     private static class JaxbMarshallerHolder {
-        static final Marshaller MARSHALLER_XMLFP;
-
-        static {
-            try {
-                MARSHALLER_XMLFP = JaxbContextHolder.JAXB_CONTEXT.createMarshaller();
-            } catch (JAXBException e) {
-                throw new RuntimeException("Error creating JAXB marshallers");
-            }
-
-        }
+        static final Marshaller MARSHALLER_XMLFP = newMarshaller();
 
         static List<Marshaller> all() {
             return Collections.unmodifiableList(Arrays.asList(
@@ -58,14 +135,30 @@ class XmlFpContext {
     }
 
     private static class JaxbUnmarshallerHolder {
-        static final Unmarshaller UNMARSHALLER_XMLFP;
+        static final Unmarshaller UNMARSHALLER_XMLFP = newUnmarshaller();
+    }
 
-        static {
-            try {
-                UNMARSHALLER_XMLFP = JaxbContextHolder.JAXB_CONTEXT.createUnmarshaller();
-            } catch (JAXBException e) {
-                throw new RuntimeException("Error creating unmarshallers", e);
-            }
+    private static class ValidatingMarshallerHolder {
+        static final Marshaller MARSHALLER = newValidatingMarshaller();
+    }
+
+    private static class ValidatingUnmarshallerHolder {
+        static final Unmarshaller UNMARSHALLER = newValidatingUnmarshaller();
+    }
+
+    private static Marshaller newMarshaller() {
+        try {
+            return JaxbContextHolder.JAXB_CONTEXT.createMarshaller();
+        } catch (JAXBException e) {
+            throw new RuntimeException("Error creating JAXB marshallers");
+        }
+    }
+
+    private static Unmarshaller newUnmarshaller() {
+        try {
+            return JaxbContextHolder.JAXB_CONTEXT.createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new RuntimeException("Error creating unmarshallers", e);
         }
     }
 }
