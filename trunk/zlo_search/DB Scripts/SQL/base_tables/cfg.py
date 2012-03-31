9a37710ba7ab4b7c491d12450eed78d31e9d4921
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-03-31 15:03:31 +0000

    idx by parentNum

--- trunk/zlo_search/DB Scripts/SQL/base_tables/cfg.py
+++ trunk/zlo_search/DB Scripts/SQL/base_tables/cfg.py
@@ -1,8 +1,11 @@
-import glob
-from os.path import split, splitext
+#import glob
+#from os.path import split, splitext
+import re
 
-CONF_D = '../../../src/main/java/info/xonix/zlo/search/config/forums'
+#CONF_D = '../../../src/main/java/info/xonix/zlo/search/config/forums'
+FORUMS_XML = '../../../src/main/java/info/xonix/zlo/search/config/forums/beans.xml'
 
+"""
 def globs(gg):
     res = []
     for g in gg:
@@ -17,6 +20,10 @@ ALL_SITE_NAMES = [
         '%s/*/*.properties' % CONF_D])
     if 'db.daemon.period.to.scan' in open(p).read()
 ]
+"""
+
+ALL_SITE_NAMES = [m for m in re.findall(
+    r'''<constructor-arg value="\d+"/>\s+<constructor-arg value="(.+?)"''', open(FORUMS_XML).read()) ]
 
 if __name__ == '__main__':
     print ALL_SITE_NAMES
