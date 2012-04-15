99ffb9ce68ea250cf2ea1a251584e4a425c2bc6c
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-04-15 13:07:27 +0000

    lucene version upd, russian analyzer : use stop-words list from old version

--- trunk/zlo_search/src/main/java/info/xonix/zlo/search/analyzers/RussianAnalyzerImproved.java
+++ trunk/zlo_search/src/main/java/info/xonix/zlo/search/analyzers/RussianAnalyzerImproved.java
@@ -7,6 +7,8 @@ import org.apache.lucene.analysis.ru.RussianAnalyzer;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * User: Vovan
@@ -14,7 +16,21 @@ import java.io.Reader;
  * Time: 16:19
  */
 public class RussianAnalyzerImproved extends Analyzer {
-    private RussianAnalyzer russianAnalyzer = new RussianAnalyzer(LuceneVersion.VERSION);
+    private static final String[] RUSSIAN_STOP_WORDS_30 = {
+            "а", "без", "более", "бы", "был", "была", "были", "было", "быть", "в",
+            "вам", "вас", "весь", "во", "вот", "все", "всего", "всех", "вы", "где",
+            "да", "даже", "для", "до", "его", "ее", "ей", "ею", "если", "есть",
+            "еще", "же", "за", "здесь", "и", "из", "или", "им", "их", "к", "как",
+            "ко", "когда", "кто", "ли", "либо", "мне", "может", "мы", "на", "надо",
+            "наш", "не", "него", "нее", "нет", "ни", "них", "но", "ну", "о", "об",
+            "однако", "он", "она", "они", "оно", "от", "очень", "по", "под", "при",
+            "с", "со", "так", "также", "такой", "там", "те", "тем", "то", "того",
+            "тоже", "той", "только", "том", "ты", "у", "уже", "хотя", "чего", "чей",
+            "чем", "что", "чтобы", "чье", "чья", "эта", "эти", "это", "я"
+    };
+
+    private RussianAnalyzer russianAnalyzer = new RussianAnalyzer(LuceneVersion.VERSION,
+            new HashSet<Object>(Arrays.asList(RUSSIAN_STOP_WORDS_30)));
 
     @Override
     public TokenStream tokenStream(String fieldName, Reader reader) {
