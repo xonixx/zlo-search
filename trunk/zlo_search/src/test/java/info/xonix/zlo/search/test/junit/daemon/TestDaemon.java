6fb6e3d01c1489b42a7ab571840dce567f172b0d
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-04-16 12:01:17 +0000

    test daemon

---
+++ trunk/zlo_search/src/test/java/info/xonix/zlo/search/test/junit/daemon/TestDaemon.java
@@ -0,0 +1,65 @@
+package info.xonix.zlo.search.test.junit.daemon;
+
+import info.xonix.zlo.search.daemon.Daemon;
+import org.apache.log4j.Logger;
+
+/**
+ * User: gubarkov
+ * Date: 16.04.12
+ * Time: 15:53
+ */
+public class TestDaemon extends Daemon{
+    private final Logger log = Logger.getLogger("testDaemon");
+    private int start;
+    private int stop;
+
+    public TestDaemon(String forumId, int start, int stop) {
+        super(forumId);
+
+        this.start = start;
+        this.stop = stop;
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
+    }
+
+    @Override
+    protected Daemon.Process createProcess() {
+        return new Daemon.Process() {
+            @Override
+            protected int getFromIndex() throws Exception {
+                log.info("getFromIndex, start=" + start);
+                return start;
+            }
+
+            @Override
+            protected int getEndIndex() throws Exception {
+                log.info("getEndIndex, stop=" + stop);
+                return stop;
+            }
+
+            @Override
+            protected void perform(int from, int to) throws Exception {
+                log.info("perform from=" + from + " to=" + to);
+            }
+
+            @Override
+            protected boolean processException(Exception ex) {
+                log.error("processException", ex);
+                return false;
+            }
+
+            @Override
+            protected void cleanUp() {
+                log.info("cleanUp");
+            }
+        };
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+}
