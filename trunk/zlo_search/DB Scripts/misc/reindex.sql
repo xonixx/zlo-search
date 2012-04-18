a57ce734b12266f39d65b88a27efb7fcb14d46a4
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2012-04-18 19:40:37 +0000

    turn off old sites, temp enable x (to reindex)

--- trunk/zlo_search/DB Scripts/misc/reindex.sql
+++ trunk/zlo_search/DB Scripts/misc/reindex.sql
@@ -4,7 +4,7 @@ update x_mipt_cc_db_dict set int_val=0 where var_name='lastIndexedDouble';
 -- update games_db_dict set int_val=0 where var_name='lastIndexedDouble';
 update velo_db_dict set int_val=0 where var_name='lastIndexedDouble';
 update dev_db_dict set int_val=0 where var_name='lastIndexedDouble';
--- update x_db_dict set int_val=0 where var_name='lastIndexedDouble';
+update x_db_dict set int_val=0 where var_name='lastIndexedDouble';
 update dolgopa_db_dict set int_val=0 where var_name='lastIndexedDouble';
 
 -- update sport_db_dict set int_val=0 where var_name='lastIndexedDouble';
