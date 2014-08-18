f355ce6fad4b416ec7d30e0e3ff397a00888d8c6
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2014-08-18 15:02:41 +0000

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SERV=xonix@rt.mipt.ru
+TOMCAT=/usr/local/apache-tomcat-7.0/
+WAR=$(ls -v zlo_web/target/*.war | tail -n 1)
+BKP=$(date +%y%m%d%H%M)
+
+WA=$TOMCAT/webapps
+
+ssh $SERV "
+echo 'Stop...'
+
+$TOMCAT/bin/shutdown.sh; sleep 10; killall -9 java
+
+echo 'Clean/Backup...'
+
+rm -rf $WA/ROOT
+mv $WA/ROOT.war $WA/ROOT.war_$BKP
+
+rm -rf $TOMCAT/work/*
+"
+
+scp $WAR $SERV:$WA/ROOT.war
+
+ssh $SERV "
+echo 'Starting...'
+
+CATALINA_OPTS='-server -Xmx300m'
+export CATALINA_OPTS
+
+$TOMCAT/bin/startup.sh;
+
+tail -f $TOMCAT/logs/catalina.out
+"
