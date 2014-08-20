7f496d68749a526554be803cf4d407636e210df9
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2014-08-20 20:17:49 +0000

    work on charts started

---
+++ trunk/zlo_search/src/main/java/info/xonix/zlo/search/charts/ChartType.java
@@ -0,0 +1,12 @@
+package info.xonix.zlo.search.charts;
+
+/**
+ * User: xonix
+ * Date: 8/20/14
+ * Time: 8:47 PM
+ */
+public enum ChartType {
+    ByHour,
+    ByWeekDay,
+    DayInterval
+}
--- trunk/zlo_search/src/main/java/info/xonix/zlo/search/config/DateFormats.java
+++ trunk/zlo_search/src/main/java/info/xonix/zlo/search/config/DateFormats.java
@@ -2,6 +2,7 @@ package info.xonix.zlo.search.config;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * User: Vovan
@@ -9,28 +10,20 @@ import java.text.SimpleDateFormat;
  * Time: 22:10:32
  */
 public class DateFormats {
-    public static final ThreadLocal<DateFormat> ddMMyyyyy_HHmm = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        }
-    };
-    public static final ThreadLocal<DateFormat> ddMMyyyy = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat("dd/MM/yyyy");
-        }
-    };
-    public static final ThreadLocal<DateFormat> ddMMyyyy_dots = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat("dd.MM.yyyy");
-        }
-    };
-    public static final ThreadLocal<DateFormat> DF_BOARD_MSG = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat("M d HH:mm:ss yyyy");
-        }
-    };
+    public static final ThreadLocal<DateFormat> ddMMyyyyy_HHmm = df("dd/MM/yyyy HH:mm");
+    public static final ThreadLocal<DateFormat> ddMMyyyy = df("dd/MM/yyyy");
+    public static final ThreadLocal<DateFormat> ddMMyyyy_dots = df("dd.MM.yyyy");
+    public static final ThreadLocal<DateFormat> DF_BOARD_MSG = df("M d HH:mm:ss yyyy");
+
+    public static final ThreadLocal<DateFormat> Hour = df("HH");
+    public static final ThreadLocal<DateFormat> WeekDay = df("E");
+
+    private static ThreadLocal<DateFormat> df(final String format) {
+        return new ThreadLocal<DateFormat>() {
+            @Override
+            protected DateFormat initialValue() {
+                return new SimpleDateFormat(format, Locale.US);
+            }
+        };
+    }
 }
