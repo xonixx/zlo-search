d3719f6bd6a656e3508bee8601df8637d80c4e00
xonixx@gmail.com <xonixx@gmail.com@cfdea282-7ff3-25da-2fc2-6cd4b62fd907>
2014-08-24 13:02:57 +0000

    more work on charts

--- zlo_web/src/main/webapp/js/charts.js
+++ zlo_web/src/main/webapp/js/charts.js
@@ -1,4 +1,4 @@
-angular.module('charts', ['common', 'ngResource', 'mgcrea.ngStrap'])
+angular.module('charts', ['common', 'ngResource', 'mgcrea.ngStrap', 'highcharts-ng'])
     .factory('Chart', function ($resource) {
         return $resource('/a/charts/:action/:id', {}, {
             submitTask: {method: 'POST', isArray: false, params: {action: 'submitTask'}},
@@ -6,23 +6,80 @@ angular.module('charts', ['common', 'ngResource', 'mgcrea.ngStrap'])
         });
     });
 
-function ChartsCtrl($scope, Chart, $timeout) {
+function ChartsCtrl($scope, Chart, $timeout, dateFilter) {
     $scope.task = {forumId: "zlo", dbNicks: "xonix", start: new Date(1388612189000), end: new Date(), type: 'ByHour'};
     $scope.submitTask = function (task) {
+        delete task.nicks;
+        delete task.result;
+        delete task.error;
+
         Chart.submitTask(task, function (res) {
-            $timeout(function () { checkTask(res.id) }, 1000)
+            $timeout(function () {
+                checkTask(res.id)
+            }, 1000)
         });
     };
 
     function checkTask(id) {
-        Chart.checkTask({id:id}, function (res) {
+        Chart.checkTask({id: id}, function (res) {
             if (res.result || res.error) {
+                $scope.chartConfig = null;
                 $scope.task = res;
-                if (res.result)
-                    res.result = angular.fromJson(res.result)
+                if (res.result) {
+                    res.result = angular.fromJson(res.result);
+                    prepareChart(res)
+                }
             } else {
-                $timeout(function () { checkTask(id) }, 2000)
+                $timeout(function () {
+                    checkTask(id)
+                }, 2000)
             }
         })
+    }
+
+    var df1 = function (inp) {
+        return dateFilter(inp, 'dd.MM.yyyy')
+    };
+
+    function prepareChart(chartTask) {
+        var type = chartTask.type;
+        var title = 'Активность ' + (
+            type == 'ByHour' ? 'по часам дня' :
+                type == 'ByWeekDay' ? 'по дням недели' :
+                    type == 'DayInterval' ? 'по дням' : '') + ' пользователя ' + chartTask.dbNicks +
+            ' в интевале дат от ' + df1(chartTask.start) + ' до ' + df1(chartTask.end);
+
+        var categories = [];
+        var data = [];
+
+        angular.forEach(chartTask.result, function (v, k) {
+            data.push(v);
+            categories.push(k);
+        });
+
+        $scope.chartConfig = {
+            title: {
+                text: title
+            },
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                title: { text: 'Число сообщений' },
+                plotLines: [
+                    {
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }
+                ]
+            },
+            series: [
+                {
+                    name: chartTask.dbNicks,
+                    data: data
+                }
+            ]
+        }
     }
 }
