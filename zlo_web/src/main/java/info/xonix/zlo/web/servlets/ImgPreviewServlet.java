0cac75a190563a1aeffd95f1543a50b5e11c894c
xonix <xonixx@gmail.com>
2016-07-24 22:24:08 +0300

    jetty ver upd fix

---
+++ zlo_web/src/main/java/info/xonix/zlo/web/servlets/ImgPreviewServlet.java
@@ -0,0 +1,14 @@
+package info.xonix.zlo.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ImgPreviewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
